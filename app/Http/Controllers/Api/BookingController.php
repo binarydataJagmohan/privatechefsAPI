0d0fbd2fabbb84b29c059e29212a353ee497dab5
Binarydata12 <binarydata.sumeet2@gmail.com>
2023-05-01 18:15:11 +0530

    working on receipt by kamal

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -17,8 +17,8 @@ class BookingController extends Controller
     public function save_booking(Request $request)
     {
         try {
-            
-            if($request->user_id){
+
+            if ($request->user_id) {
 
                 $user = User::find($request->user_id);
                 $user->name = $request->name;
@@ -27,15 +27,15 @@ class BookingController extends Controller
                 $user->phone = $request->phone;
                 $savedata = $user->save();
 
-                if($savedata){
+                if ($savedata) {
 
                     $booking = new Booking();
                     $booking->user_id = $request->user_id;
                     $booking->service_id = $request->service_id;
-                    $booking->cuisine_id = implode(",",$request->cuisine_id);
+                    $booking->cuisine_id = implode(",", $request->cuisine_id);
 
-                    if($request->allergies_id){
-                        $booking->allergies_id = implode(",",$request->allergies_id);    
+                    if ($request->allergies_id) {
+                        $booking->allergies_id = implode(",", $request->allergies_id);
                     }
 
                     $booking->notes = $request->notes;
@@ -46,93 +46,9 @@ class BookingController extends Controller
 
                     $savebookingdata = $booking->save();
 
-                    if($savebookingdata) {
+                    if ($savebookingdata) {
 
-                        if($request->category == 'onetime'){
-
-                            $dateString = $request->date;
-                            $timezoneStart = strpos($dateString, '(');
-                            $timezoneEnd = strpos($dateString, ')');
-                            $dateString = substr_replace($dateString, '', $timezoneStart, $timezoneEnd - $timezoneStart + 1);
-                            $date = Carbon::parse($dateString);
-                            $formattedDate = $date->format('Y-m-d');
-
-                        
-                            $bookingmeals = new BookingMeals();
-                            $bookingmeals->booking_id = $booking->id;
-                            $bookingmeals->date = $formattedDate;
-                            $bookingmeals->breakfast = $request->meals['breakfast'] == '1' ? 'yes' : 'no';
-                            $bookingmeals->lunch = $request->meals['lunch'] == '1' ? 'yes' : 'no';
-                            $bookingmeals->dinner = $request->meals['dinner'] == '1' ? 'yes' : 'no';
-                            $bookingmeals->category = $request->category;
-                            $savebookingmeals  = $bookingmeals->save();
-
-                        }else {
-
-                            foreach($request->meals as $meals){
-
-                               $bookingmeals = new BookingMeals();
-                                $bookingmeals->booking_id = $booking->id;
-                                $bookingmeals->date = \Carbon\Carbon::createFromFormat('d/m/Y', $meals['date'])->format('Y-m-d');
-                                $bookingmeals->breakfast = $meals['breakfast'] == '1' ? 'yes' : 'no';
-                                $bookingmeals->lunch = $meals['lunch'] == '1' ? 'yes' : 'no';
-                                $bookingmeals->dinner = $meals['dinner'] == '1' ? 'yes' : 'no';
-                                $bookingmeals->category = $request->category;
-                                $savebookingmeals  = $bookingmeals->save();
-                            }
-
-
-                        }
-
-                        
-                    }
-
-                    return response()->json(['status' => true, 'message' => "booking done successfully", 'bookingid' => $booking->id], 200);
-
-                }
-
-            }else {
-
-
-            $checkemail  = User::where('email',$request->email)->count();
-
-            if($checkemail <= 0){
-
-                 
-                $password = Str::random(10);
-                $user = new User();
-                $user->name = $request->name;
-                $user->surname = $request->surname;
-                $user->email = $request->email;
-                $user->phone = $request->phone;
-                $user->password = Hash::make($password);
-                $user->view_password = $password;
-                $user->role = 'user';
-                $savedata = $user->save();
-
-                if($savedata){
-
-                    $booking = new Booking();
-                    $booking->user_id = $user->id;
-                    $booking->service_id = $request->service_id;
-                    $booking->cuisine_id = implode(",",$request->cuisine_id);
-
-                    if($request->allergies_id){
-                        $booking->allergies_id = implode(",",$request->allergies_id);    
-                    }
-
-                    $booking->notes = $request->notes;
-                    $booking->location = $request->location;
-                    $booking->adults = $request->adults ? $request->adults : 0;
-                    $booking->childrens = $request->childrens ? $request->childrens : 0;
-                    $booking->teens = $request->teens ? $request->teens : 0;
-
-                    $savebookingdata = $booking->save();
-
-                    
-                    if($savebookingdata) {
-
-                        if($request->category == 'onetime'){
+                        if ($request->category == 'onetime') {
 
                             $dateString = $request->date;
                             $timezoneStart = strpos($dateString, '(');
@@ -141,7 +57,7 @@ class BookingController extends Controller
                             $date = Carbon::parse($dateString);
                             $formattedDate = $date->format('Y-m-d');
 
-                        
+
                             $bookingmeals = new BookingMeals();
                             $bookingmeals->booking_id = $booking->id;
                             $bookingmeals->date = $formattedDate;
@@ -150,10 +66,9 @@ class BookingController extends Controller
                             $bookingmeals->dinner = $request->meals['dinner'] == '1' ? 'yes' : 'no';
                             $bookingmeals->category = $request->category;
                             $savebookingmeals  = $bookingmeals->save();
+                        } else {
 
-                        }else {
-
-                            foreach($request->meals as $meals){
+                            foreach ($request->meals as $meals) {
 
                                 $bookingmeals = new BookingMeals();
                                 $bookingmeals->booking_id = $booking->id;
@@ -164,58 +79,145 @@ class BookingController extends Controller
                                 $bookingmeals->category = $request->category;
                                 $savebookingmeals  = $bookingmeals->save();
                             }
-
                         }
                     }
 
                     return response()->json(['status' => true, 'message' => "booking done successfully", 'bookingid' => $booking->id], 200);
-
                 }
+            } else {
 
-            
-                
-            }else {
 
-                return response()->json(['status' => false, 'message' => "Email already exits", 'data' => ""], 200);
+                $checkemail  = User::where('email', $request->email)->count();
+
+                if ($checkemail <= 0) {
+
+
+                    $password = Str::random(10);
+                    $user = new User();
+                    $user->name = $request->name;
+                    $user->surname = $request->surname;
+                    $user->email = $request->email;
+                    $user->phone = $request->phone;
+                    $user->password = Hash::make($password);
+                    $user->view_password = $password;
+                    $user->role = 'user';
+                    $savedata = $user->save();
+
+                    if ($savedata) {
+
+                        $booking = new Booking();
+                        $booking->user_id = $user->id;
+                        $booking->service_id = $request->service_id;
+                        $booking->cuisine_id = implode(",", $request->cuisine_id);
+
+                        if ($request->allergies_id) {
+                            $booking->allergies_id = implode(",", $request->allergies_id);
+                        }
+
+                        $booking->notes = $request->notes;
+                        $booking->location = $request->location;
+                        $booking->adults = $request->adults ? $request->adults : 0;
+                        $booking->childrens = $request->childrens ? $request->childrens : 0;
+                        $booking->teens = $request->teens ? $request->teens : 0;
+
+                        $savebookingdata = $booking->save();
+
+
+                        if ($savebookingdata) {
+
+                            if ($request->category == 'onetime') {
+
+                                $dateString = $request->date;
+                                $timezoneStart = strpos($dateString, '(');
+                                $timezoneEnd = strpos($dateString, ')');
+                                $dateString = substr_replace($dateString, '', $timezoneStart, $timezoneEnd - $timezoneStart + 1);
+                                $date = Carbon::parse($dateString);
+                                $formattedDate = $date->format('Y-m-d');
+
+
+                                $bookingmeals = new BookingMeals();
+                                $bookingmeals->booking_id = $booking->id;
+                                $bookingmeals->date = $formattedDate;
+                                $bookingmeals->breakfast = $request->meals['breakfast'] == '1' ? 'yes' : 'no';
+                                $bookingmeals->lunch = $request->meals['lunch'] == '1' ? 'yes' : 'no';
+                                $bookingmeals->dinner = $request->meals['dinner'] == '1' ? 'yes' : 'no';
+                                $bookingmeals->category = $request->category;
+                                $savebookingmeals  = $bookingmeals->save();
+                            } else {
+
+                                foreach ($request->meals as $meals) {
+
+                                    $bookingmeals = new BookingMeals();
+                                    $bookingmeals->booking_id = $booking->id;
+                                    $bookingmeals->date = \Carbon\Carbon::createFromFormat('d/m/Y', $meals['date'])->format('Y-m-d');
+                                    $bookingmeals->breakfast = $meals['breakfast'] == '1' ? 'yes' : 'no';
+                                    $bookingmeals->lunch = $meals['lunch'] == '1' ? 'yes' : 'no';
+                                    $bookingmeals->dinner = $meals['dinner'] == '1' ? 'yes' : 'no';
+                                    $bookingmeals->category = $request->category;
+                                    $savebookingmeals  = $bookingmeals->save();
+                                }
+                            }
+                        }
+
+                        return response()->json(['status' => true, 'message' => "booking done successfully", 'bookingid' => $booking->id], 200);
+                    }
+                } else {
+
+                    return response()->json(['status' => false, 'message' => "Email already exits", 'data' => ""], 200);
+                }
             }
-
-        }
         } catch (\Exception $e) {
             throw new HttpException(500, $e->getMessage());
         }
     }
 
-   public function get_User_By_Booking()
-{
-    $user = DB::table('users')
-        ->join('bookings', 'users.id', '=', 'bookings.user_id')
-        ->join('booking_meals', 'bookings.id', '=', 'booking_meals.booking_id')
-        ->select('users.name','users.id','users.surname','users.address','users.email','users.phone','bookings.booking_status','booking_meals.category','booking_meals.date','bookings.adults','bookings.teens','bookings.childrens','booking_meals.created_at')
-        //->groupBy('users.id')
-        ->get();
-        
-    if (!$user) {
-        return response()->json(['message' => 'Booking not found','status'=>true], 404);
+    public function get_User_By_Booking()
+    {
+        $user = DB::table('users')
+            ->join('bookings', 'users.id', '=', 'bookings.user_id')
+            ->join('booking_meals', 'bookings.id', '=', 'booking_meals.booking_id')
+            ->select('users.name', 'users.id', 'users.surname', 'users.address', 'users.email', 'users.phone', 'bookings.booking_status', 'booking_meals.category', 'booking_meals.date', 'bookings.adults', 'bookings.teens', 'bookings.childrens', 'booking_meals.created_at')
+            //->groupBy('users.id')
+            ->get();
+
+        if (!$user) {
+            return response()->json(['message' => 'Booking not found', 'status' => true], 404);
+        }
+
+        return response()->json(['status' => true, 'message' => 'Data fetched', 'data' => $user]);
     }
 
-    return response()->json(['status'=>true,'message'=>'Data fetched','data'=>$user]);
-}
+    public function get_User_By_Booking_Id($id)
+    {
+        $user = DB::table('users')
+            ->join('bookings', 'users.id', '=', 'bookings.user_id')
+            ->join('booking_meals', 'bookings.id', '=', 'booking_meals.booking_id')
+            ->select('users.name', 'users.id', 'users.surname', 'users.address', 'users.email', 'users.phone', 'bookings.booking_status', 'booking_meals.category', 'booking_meals.date', 'bookings.adults', 'bookings.teens', 'bookings.childrens', 'booking_meals.created_at', 'bookings.id')
+            ->where('users.id', '=', $id)
+            ->first();
 
-public function get_User_By_Booking_Id($id)
-{
-    $user = DB::table('users')
-        ->join('bookings', 'users.id', '=', 'bookings.user_id')
-        ->join('booking_meals', 'bookings.id', '=', 'booking_meals.booking_id')
-        ->select('users.name','users.id','users.surname','users.address','users.email','users.phone','bookings.booking_status','booking_meals.category','booking_meals.date','bookings.adults','bookings.teens','bookings.childrens','booking_meals.created_at','bookings.id')
-        ->where('users.id', '=', $id)
-        ->first();
-        
-    if (!$user) {
-        return response()->json(['message' => 'Booking not found','status'=>true], 404);
+        if (!$user) {
+            return response()->json(['message' => 'Booking not found', 'status' => true], 404);
+        }
+
+        return response()->json(['status' => true, 'message' => 'Data fetched', 'data' => $user]);
     }
-
-    return response()->json(['status'=>true,'message'=>'Data fetched','data'=>$user]);
-}
-
-
+    public function get_all_booking()
+    {
+        try {
+            $bookings = Booking::where('status', 'active')->get();
+            return response()->json([
+                'status' => true,
+                'message' => 'All Bookings fetched successfully.',
+                'data' => $bookings
+            ],200);
+        } catch (\Exception $e) {
+            throw new HttpException(500, $e->getMessage());
+            return response()->json([
+                'status' => false,
+                'message' => 'Booking not found',
+                'data' => ''
+            ], 404);
+        }
+    }
 }
