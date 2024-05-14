548e8366ec5a09a2962b865499dc119d6320888a
devbinary003 <binarydata.sale@gmail.com>
2024-05-14 19:35:30 +0530

    fixed emails subject text remove changes code upload
    fixed emails subject text remove changes code upload

--- resources/views/emails/chat_messages.blade.php
+++ resources/views/emails/chat_messages.blade.php
@@ -61,12 +61,12 @@
                                          </td>
                                     </tr>
 
-                                    <tr>
+                                    <!-- <tr>
                                         <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
                                             valign="top">
                                             Subject: New Message Alert - Private Chefs World
                                         </td>
-                                    </tr>
+                                    </tr> -->
 
                                     <tr
                                         style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
